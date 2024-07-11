797966caa9b66097cf06c455994eb92def05cc52
Aashish <135721204+Codewiizard@users.noreply.github.com>
2024-07-11 12:31:29 +0530

    Delete module1 directory

--- module1/migrations/2_deploy_migration.js
+++
@@ -1,6 +0,0 @@
-var HelloWorld = artifacts.require("HelloWorld");
-var Assessment = artifacts.require("Assessment")
-module.exports = function(deployer) {
-  deployer.deploy(HelloWorld);
-  deployer.deploy(Assessment);
-}
